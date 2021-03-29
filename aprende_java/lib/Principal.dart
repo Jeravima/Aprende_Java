c4c33cd4040b1ce5ba7b30b75575f9e591128756
jeravima <jeravi61@gmail.com>
2021-03-29 14:50:14 -0500

    Probando ando

---
+++ aprende_java/lib/Principal.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Principal extends StatelessWidget {
+  const Principal({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Principal"),
+
+        
+
+      ),
+    );
+  }
+}
--- aprende_java/lib/main.dart
+++ aprende_java/lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:aprende_java/Principal.dart';
 import 'package:flutter/material.dart';
- 
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,8 +13,19 @@ class MyApp extends StatelessWidget {
           title: Text('Material App Bar'),
         ),
         body: Center(
-          child: Container(
-            child: Text('Hello World'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // ignore: deprecated_member_use
+              RaisedButton(
+                  child: Text("Pagina Principal"),
+                  onPressed: () => {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => Principal()))
+                      })
+            ],
           ),
         ),
       ),
